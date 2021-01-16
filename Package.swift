07d68d53beb9cc3eaef0ccdcbda63afde77912a6
Franklin Cruz <daisuke.sysoft@gmail.com>
2021-01-16 15:54:08 -0300

    Activate swift actions

--- Package.swift
+++ Package.swift
@@ -6,7 +6,6 @@ import PackageDescription
 let package = Package(
     name: "Rosetta",
     products: [
-        // Products define the executables and libraries a package produces, and make them visible to other packages.
         .executable(
             name: "Monkey",
             targets: ["Monkey"]),
@@ -19,8 +18,6 @@ let package = Package(
     ],
     dependencies: [ ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "Monkey",
             dependencies: [
